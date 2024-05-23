2fa1cbcae8e4544abb613247f70b51e3f7d685a3
GeetanshGandhi <76564148+GeetanshGandhi@users.noreply.github.com>
2024-05-23 11:38:17 +0000

    portfolio-update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Silverline Hospital: DBMS application",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Silverline Hospital is a semi-GUI driven DBMS application which constitutes all relevant functionalities and operations performed in a hospital setting and system, right from a patient's admission to it's' discharge. The project focused mainly on database management. Skills used: Python as frontend(shell+Tkinter) and backend, MySQL for database and Canva for graphic designing.",
+    url: "https://github.com/GeetanshGandhi/SilverlineHospitals.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Gada Electronics: e-Commerce application",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Gada Electronics is an application build purely using Java as an e-Commerce application for electronic products like Smartphones, etc. The project majorly focused on interactive user experience and interface. The main aim of the project was to have a grip on concepts of Object oriented paradigm of computer programming.\nTechnologies used: java as backend, javax Swing and java AWT as frontend, MySQL as database.",
+    url: "https://github.com/GeetanshGandhi/GadaElectronics.git",
   },
   {
-    title: "My Resume Site",
+    title: "Portfolio site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A responsive website of resume created using pure HTML/CSS and deployed using Github pages.",
+    url: "https://geetanshgandhi.github.io/PortfolioCSSonly/",
   },
   {
     title: "GitHub Codespaces and github.dev",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A rather simple application build using Python where an input image object is manipulated using opencv library and converted into a sketch.",
+    url: "https://github.com/GeetanshGandhi/Image-to-Sketch-Convertor.git",
   },
 ];
 
